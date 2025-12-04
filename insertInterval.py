4f0f32bb2d5081f5a6e8345f792e9f7fa53dcd49
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-03 20:51:31 -0600

    Add initial structure for insertInterval method

--- insertInterval.py
+++ insertInterval.py
@@ -4,3 +4,8 @@ from typing import List
 
 class Solution:
     def insertInterval(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
