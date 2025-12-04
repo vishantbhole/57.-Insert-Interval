78da445d00fbda0968289ed9b813436095413928
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-03 20:52:52 -0600

    Fix insertInterval function to append newInterval

--- insertInterval.py
+++ insertInterval.py
@@ -15,4 +15,6 @@ class Solution:
                 res.append(intervals[i])
             else:
                 newInterval = [min(newInterval[0],intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        res.append(newInterval)
+        return res
         
